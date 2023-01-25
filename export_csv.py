32472637c8f71d1dfee5f051d5fb62bc61a823f6
Petit Francois <francois.petit@student-cs.fr>
2023-01-25 17:16:33 +0100

    csv

--- export_csv.py
+++ export_csv.py
@@ -1,30 +1,35 @@
 import csv
 import pickle as pkl
 
-annee=2023
-dic_rho=pkl.load(open("dic_rho.p", "rb"))
-rho=pkl.load(open("rho.p", "rb"))
-etats=pkl.load(open("etats.p","rb"))
-previsions=pkl.load(open("previsions.p", "rb"))
-rho=1
+annee = 2023
+dic_rho = pkl.load(open("dic_rho.p", "rb"))
+rho = pkl.load(open("rho.p", "rb"))
+etats = pkl.load(open("etats.p", "rb"))
+previsions = pkl.load(open("previsions.p", "rb"))
 
 with open("exports/export_{}.csv".format(annee), 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
     # colonnes
-    writer.writerow(('secteur', 'site', 'debit_prevu','sature',"700 MHz", "800 MHz", "1800 MHz", "2100 MHz", "2600 MHz", "3500 MHz"))
+    writer.writerow(('secteur', 'site', 'debit_prevu', 'sature', "700 MHz",
+                    "800 MHz", "1800 MHz", "2100 MHz", "2600 MHz", "3500 MHz"))
     # lignes
     for secteur in etats:
-        if dic_rho[secteur][annee]>rho:
+        if dic_rho[secteur][annee] > rho:
             sature = 1
         else:
-            sature=0
-        etat=[]
-        freq = ["700 MHz", "800 MHz", "1800 MHz", "2100 MHz", "2600 MHz", "3500 MHz"]
-        for i in range(len(freq)):
+            sature = 0
+        etat = []
+        freq = ["700 MHz", "800 MHz", "1800 MHz",
+                "2100 MHz", "2600 MHz", "3500 MHz"]
+        for i in range(len(freq)-1):
             if etats[secteur][freq[i]]:
-                etat.append(1)
+                etat.append("4G")
             else:
                 etat.append(0)
+        if etats[secteur]["3500 MHz"]:
+            etat.append("5G")
+        else:
+            etat.append(0)
 
-        writer.writerow((secteur, secteur[0:6],previsions[secteur][annee], sature, etat[0], etat[1], etat[2],etat[3],etat[4],etat[5]))
-
+        writer.writerow((secteur, secteur[0:6], previsions[secteur][annee],
+                        sature, etat[0], etat[1], etat[2], etat[3], etat[4], etat[5]))
